428aa2d465e20829fe9c7bc61fa809eb0cf68451
Vivek <vivek2511singh95@gmail.com>
2025-05-27 17:39:26 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
   $('.nav-link').on('click', function (e) {
-    e.preventDefault(); // Prevent default if links are "#"
-
-    // Remove active class from all
+    e.preventDefault();
     $('.nav-link').removeClass('active');
-
-    // Add active class to clicked item
     $(this).addClass('active');
   });
 });
